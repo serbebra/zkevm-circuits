e42d12951d8e9947d2d7334350b7cd377f665247
lightsing <light.tsing@gmail.com>
2023-07-31 17:34:19 +0800

    add range check

--- gadgets/src/less_than.rs
+++ gadgets/src/less_than.rs
@@ -3,16 +3,14 @@
 use eth_types::Field;
 use halo2_proofs::{
     arithmetic::FieldExt,
-    circuit::{Chip, Region, Value},
-    plonk::{Advice, Column, ConstraintSystem, Error, Expression, VirtualCells},
+    circuit::{Chip, Layouter, Region, Value},
+    plonk::{Advice, Column, ConstraintSystem, Error, Expression, Fixed, VirtualCells},
     poly::Rotation,
 };
 
-use crate::util::sum;
-
-use super::{
+use crate::{
     bool_check,
-    util::{expr_from_bytes, pow_of_two},
+    util::{expr_from_bytes, pow_of_two, sum},
 };
 
 /// Instruction that the Lt chip needs to implement.
@@ -25,6 +23,9 @@ pub trait LtInstruction<F: FieldExt> {
         lhs: F,
         rhs: F,
     ) -> Result<(), Error>;
+
+    /// Load the u8x2 lookup table.
+    fn load(&self, layouter: &mut impl Layouter<F>) -> Result<(), Error>;
 }
 
 /// Config for the Lt chip.
@@ -33,8 +34,9 @@ pub struct LtConfig<F, const N_BYTES: usize> {
     /// Denotes the lt outcome. If lhs < rhs then lt == 1, otherwise lt == 0.
     pub lt: Column<Advice>,
     /// Denotes the bytes representation of the difference between lhs and rhs.
-    /// Note that the range of each byte is not checked by this config.
     pub diff: [Column<Advice>; N_BYTES],
+    /// Denotes the range within which each byte should lie.
+    pub u8x2: Column<Fixed>,
     /// Denotes the range within which both lhs and rhs lie.
     pub range: F,
 }
@@ -68,6 +70,7 @@ impl<F: Field, const N_BYTES: usize> LtChip<F, N_BYTES> {
     ) -> LtConfig<F, N_BYTES> {
         let lt = meta.advice_column();
         let diff = [(); N_BYTES].map(|_| meta.advice_column());
+        let u8x2 = meta.fixed_column();
         let range = pow_of_two(N_BYTES * 8);
 
         meta.create_gate("lt gate", |meta| {
@@ -89,7 +92,29 @@ impl<F: Field, const N_BYTES: usize> LtChip<F, N_BYTES> {
                 .map(move |poly| q_enable.clone() * poly)
         });
 
-        LtConfig { lt, diff, range }
+        meta.annotate_lookup_any_column(u8x2, || "LOOKUP_u8x2");
+
+        for cell_columns in diff.chunks(2) {
+            meta.lookup_any("range check for u8x2", |meta| {
+                let cell_expr = if cell_columns.len() == 2 {
+                    meta.query_advice(cell_columns[0], Rotation::cur())
+                        * Expression::Constant(pow_of_two(8))
+                        + (meta.query_advice(cell_columns[1], Rotation::cur()))
+                } else {
+                    meta.query_advice(cell_columns[0], Rotation::cur())
+                        * Expression::Constant(pow_of_two(8))
+                };
+                let u8x2_range = meta.query_fixed(u8x2, Rotation::cur());
+                vec![(cell_expr, u8x2_range)]
+            });
+        }
+
+        LtConfig {
+            lt,
+            diff,
+            u8x2,
+            range,
+        }
     }
 
     /// Constructs a Lt chip given a config.
@@ -129,6 +154,25 @@ impl<F: Field, const N_BYTES: usize> LtInstruction<F> for LtChip<F, N_BYTES> {
 
         Ok(())
     }
+
+    fn load(&self, layouter: &mut impl Layouter<F>) -> Result<(), Error> {
+        const RANGE: usize = u16::MAX as usize;
+
+        layouter.assign_region(
+            || "load u8x2 range check table",
+            |mut region| {
+                for i in 0..=RANGE {
+                    region.assign_fixed(
+                        || "assign cell in fixed column",
+                        self.config.u8x2,
+                        i,
+                        || Value::known(F::from(i as u64)),
+                    )?;
+                }
+                Ok(())
+            },
+        )
+    }
 }
 
 impl<F: Field, const N_BYTES: usize> Chip<F> for LtChip<F, N_BYTES> {
@@ -164,7 +208,7 @@ mod test {
 
             // TODO: remove zk blinding factors in halo2 to restore the
             // correct k (without the extra + 2).
-            let k = usize::BITS - $values.len().leading_zeros() + 2;
+            let k = (usize::BITS - $values.len().leading_zeros() + 2).max(17);
             let circuit = TestCircuit::<Fp> {
                 values: Some($values),
                 checks: Some($checks),
@@ -181,7 +225,7 @@ mod test {
 
             // TODO: remove zk blinding factors in halo2 to restore the
             // correct k (without the extra + 2).
-            let k = usize::BITS - $values.len().leading_zeros() + 2;
+            let k = (usize::BITS - $values.len().leading_zeros() + 2).max(17);
             let circuit = TestCircuit::<Fp> {
                 values: Some($values),
                 checks: Some($checks),
@@ -264,6 +308,8 @@ mod test {
                 let checks = self.checks.as_ref().ok_or(Error::Synthesis)?;
                 let (first_value, values) = values.split_at(1);
                 let first_value = first_value[0];
+
+                chip.load(&mut layouter)?;
 
                 layouter.assign_region(
                     || "witness",
@@ -386,6 +432,8 @@ mod test {
                     })
                     .ok_or(Error::Synthesis)?;
                 let checks = self.checks.as_ref().ok_or(Error::Synthesis)?;
+
+                chip.load(&mut layouter)?;
 
                 layouter.assign_region(
                     || "witness",
