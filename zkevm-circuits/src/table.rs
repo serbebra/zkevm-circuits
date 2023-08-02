6fd777cb05d089f42b1f1a8d9786e79330c981ee
Ho Vei <noelwei@gmail.com>
2023-08-03 00:30:30 +0800

    induce tx_type to tx field

--- zkevm-circuits/src/table.rs
+++ zkevm-circuits/src/table.rs
@@ -176,6 +176,8 @@ pub enum TxFieldTag {
     TxHash,
     /// The block number in which this tx is included.
     BlockNumber,
+    /// TxType: Type of the transaction
+    TxType,
 }
 impl_expr!(TxFieldTag);
 
