fee35d64b1c2e8e766dec03cc25b40f951134cf5
Ho Vei <noelwei@gmail.com>
2023-08-18 15:40:34 +0800

    make l1 queue index default

--- eth-types/src/l2_types.rs
+++ eth-types/src/l2_types.rs
@@ -30,7 +30,7 @@ pub struct BlockTrace {
     #[serde(rename = "txStorageTraces", default)]
     pub tx_storage_trace: Vec<StorageTrace>,
     /// l1 tx queue
-    #[serde(rename = "startL1QueueIndex")]
+    #[serde(rename = "startL1QueueIndex", default)]
     pub start_l1_queue_index: u64,
 }
 
