2d5f0c9c8462e0a0b74ea4f6b0f31d9860ae21d0
Steven Gu <asongala@163.com>
2023-08-29 16:22:54 +0800

    Update script with `no-par` feature.

--- testool/run_test_ids.sh
+++ testool/run_test_ids.sh
@@ -2,5 +2,5 @@
 export RUST_BACKTRACE=1
 export RUST_LOG=trace
 
-cargo run --features scroll --release -- --suite nightly --circuits sc --test-ids test_ids.txt --report
+cargo run --features "no-par scroll"  --release -- --suite nightly --circuits sc --test-ids test_ids.txt --report
 
