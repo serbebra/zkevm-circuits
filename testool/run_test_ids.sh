e21add172e7c2bcedfc7b90b9987f891bb19ebeb
Steven Gu <asongala@163.com>
2023-09-13 06:57:40 +0800

    Update test_ids.txt.

--- testool/run_test_ids.sh
+++ testool/run_test_ids.sh
@@ -2,5 +2,5 @@
 export RUST_BACKTRACE=1
 export RUST_LOG=trace
 
-cargo run --features "scroll"  --release -- --suite nightly --circuits sc --test-ids test_ids.txt --report
+cargo run --features "parallel_syn scroll"  --release -- --suite nightly --circuits sc --test-ids test_ids.txt --report
 
