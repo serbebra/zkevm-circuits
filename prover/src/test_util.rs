3dbc0b176bdee8bc77b2ddfbb3b4bed656727f47
Steven Gu <asongala@163.com>
2023-09-13 09:21:33 +0800

    U

--- prover/src/test_util.rs
+++
@@ -1,7 +0,0 @@
-mod proof;
-
-pub use proof::{
-    gen_and_verify_batch_proofs, gen_and_verify_chunk_proofs, gen_and_verify_normal_and_evm_proofs,
-};
-
-pub const PARAMS_DIR: &str = "./test_params";
