6564e1654903c88e8c18988ae4f54db53ada51c6
lightsing <light.tsing@gmail.com>
2023-09-26 12:25:22 +0800

    add bencher

---
+++ benchmarks/benches/estimate_rows.rs
@@ -0,0 +1,5 @@
+use criterion::*;
+fn bench(c: &mut Criterion) {}
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
---
+++ benchmarks/src/utils.rs
@@ -0,0 +1,41 @@
+use std::path::Path;
+
+/// Recursive iterator over all json files in a directory.
+pub struct TracesFileIterator {
+    read_dirs: Vec<std::fs::ReadDir>,
+}
+
+impl TracesFileIterator {
+    pub fn from_dir<P: AsRef<Path>>(dir: P) -> Self {
+        Self {
+            read_dirs: vec![std::fs::read_dir(dir).expect("read dir failed")],
+        }
+    }
+}
+
+impl Iterator for TracesFileIterator {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match self.read_dirs.last_mut().unwrap().next() {
+                Some(entry) => {
+                    let entry = entry.unwrap();
+                    let path = entry.path();
+                    if path.is_dir() {
+                        self.read_dirs
+                            .push(std::fs::read_dir(path).expect("read dir failed"));
+                    } else if path.extension().map(|s| s == "json").unwrap_or(false) {
+                        return Some(std::fs::read_to_string(path).expect("read file failed"));
+                    }
+                }
+                None => {
+                    self.read_dirs.pop();
+                    if self.read_dirs.is_empty() {
+                        return None;
+                    }
+                }
+            }
+        }
+    }
+}
