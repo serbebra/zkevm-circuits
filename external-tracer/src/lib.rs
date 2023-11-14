b5147449cd98fea43fe3952b53e0310816c602c2
lightsing <light.tsing@gmail.com>
2023-11-14 16:08:21 +0800

    unbound recursion

--- external-tracer/src/lib.rs
+++ external-tracer/src/lib.rs
@@ -108,8 +108,10 @@ pub fn trace(config: &TraceConfig) -> Result<Vec<GethExecTrace>, Error> {
 
     log::trace!("trace: {}", trace_string);
 
-    let trace = serde_json::from_str(&trace_string).map_err(Error::SerdeError)?;
-    Ok(trace)
+    let mut deserializer = serde_json::Deserializer::from_str(&trace_string);
+    deserializer.disable_recursion_limit();
+    let deserializer = serde_stacker::Deserializer::new(&mut deserializer);
+    serde::Deserialize::deserialize(deserializer).map_err(Error::SerdeError)
 }
 
 /// Creates a l2-trace for the specified config
@@ -135,7 +137,10 @@ pub fn l2trace(config: &TraceConfig) -> Result<BlockTrace, Error> {
 
     log::trace!("trace: {}", trace_string);
 
-    serde_json::from_str(&trace_string).map_err(Error::SerdeError)
+    let mut deserializer = serde_json::Deserializer::from_str(&trace_string);
+    deserializer.disable_recursion_limit();
+    let deserializer = serde_stacker::Deserializer::new(&mut deserializer);
+    serde::Deserialize::deserialize(deserializer).map_err(Error::SerdeError)
 }
 
 #[cfg(feature = "scroll")]
