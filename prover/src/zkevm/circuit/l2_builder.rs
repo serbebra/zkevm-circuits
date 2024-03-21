cdb8346b2dc1d4d2a7f74b725b7e224c169959aa
Zhang Zhuo <mycinbrin@gmail.com>
2024-03-21 12:54:56 +0800

    optimize follower ccc

--- prover/src/zkevm/circuit/l2_builder.rs
+++ prover/src/zkevm/circuit/l2_builder.rs
@@ -75,8 +75,19 @@ pub fn calculate_row_usage_of_witness_block(
     );
     assert_eq!(rows[10].name, "poseidon");
     assert_eq!(rows[13].name, "mpt");
-    // empirical estimation is each row in mpt cost 1.5 hash (aka 12 rows)
-    let mpt_poseidon_rows = rows[13].row_num_real * 12;
+    // We collected real metrics from Scroll mainnet, and here is the graph
+    // https://ibb.co/gVfvW7h
+    // 6 is already very very conservative. Besides, considering a chunk consists of many txs,
+    // using this number is safe.
+    let poseidon_estimate_ratio = if witness_block.txs.len() > 1 {
+        // follower ccc
+        6
+    } else {
+        // singer ccc or single tx block follower ccc,
+        // even i think 6 is safe, here we still keep the old value
+        12
+    };
+    let mpt_poseidon_rows = rows[13].row_num_real * poseidon_estimate_ratio;
     if witness_block.mpt_updates.smt_traces.is_empty() {
         rows[10].row_num_real += mpt_poseidon_rows;
         log::debug!("calculate_row_usage_of_witness_block light mode, adding {mpt_poseidon_rows} poseidon rows");
