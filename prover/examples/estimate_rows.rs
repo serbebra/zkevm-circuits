2b2a226a3a152ae986b2f957eecfddadf4124b1c
lightsing <light.tsing@gmail.com>
2024-04-24 14:56:24 +0800

    perf

---
+++ prover/examples/estimate_rows.rs
@@ -0,0 +1,24 @@
+use prover::{
+    types::BlockTraceJsonRpcResult,
+    zkevm::circuit::{SuperCircuit, TargetCircuit},
+    BlockTrace,
+};
+use zkevm_circuits::evm_circuit::ExecutionState;
+
+fn main() {
+    let trace = std::fs::read_to_string(std::env::var("TRACE_FILE").unwrap()).unwrap();
+
+    let block_trace: BlockTrace = serde_json::from_str(&trace).unwrap();
+    let block_trace = vec![block_trace];
+    println!("{}", ExecutionState::BeginTx.get_step_height());
+    let now = std::time::Instant::now();
+    let result = estimate_rows(block_trace);
+    let elapsed = now.elapsed();
+    println!("elapsed: {elapsed:?}, result: {result}");
+}
+
+#[inline(never)]
+fn estimate_rows(trace: Vec<BlockTrace>) -> usize {
+    let result = SuperCircuit::estimate_rows(trace).unwrap();
+    result
+}
