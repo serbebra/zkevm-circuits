86229fe195b7b05ec620ace44535ec680a79e0fc
lightsing <light.tsing@gmail.com>
2024-05-14 17:43:17 +0800

    backoff on rate limit

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -10,7 +10,7 @@ use eth_types::Address;
 use ethers::{
     abi,
     core::{k256::ecdsa::SigningKey, types::Bytes},
-    prelude::JsonRpcClient,
+    prelude::{JsonRpcClient, JsonRpcError, RetryClientBuilder, RetryPolicy},
     providers::{Http, Provider},
     signers::{coins_bip39::English, MnemonicBuilder, Signer, Wallet},
 };
@@ -89,7 +89,14 @@ pub fn get_transport() -> Http {
 
 /// Get the integration test [`GethClient`]
 pub fn get_client() -> GethClient<impl JsonRpcClient> {
-    let transport = Http::new(Url::parse(&GETH0_URL).expect("invalid url"));
+    let transport = RetryClientBuilder::default()
+        .rate_limit_retries(10)
+        .timeout_retries(10)
+        .initial_backoff(Duration::from_secs(1))
+        .build(
+            Http::new(Url::parse(&GETH0_URL).expect("invalid url")),
+            Box::<HttpRateLimitRetryPolicy>::default(),
+        );
     GethClient::new(transport)
 }
 
@@ -169,3 +176,80 @@ pub struct CompiledContract {
 
 /// Common code for integration tests of circuits.
 pub mod integration_test_circuits;
+
+/// ...
+#[derive(Debug, Default)]
+pub struct HttpRateLimitRetryPolicy;
+
+impl RetryPolicy<ethers::providers::HttpClientError> for HttpRateLimitRetryPolicy {
+    fn should_retry(&self, error: &ethers::providers::HttpClientError) -> bool {
+        fn should_retry_json_rpc_error(err: &JsonRpcError) -> bool {
+            let JsonRpcError { code, message, .. } = err;
+            // alchemy throws it this way
+            if *code == 429 {
+                return true;
+            }
+
+            // This is an infura error code for `exceeded project rate limit`
+            if *code == -32005 {
+                return true;
+            }
+
+            // alternative alchemy error for specific IPs
+            if *code == -32016 && message.contains("rate limit") {
+                return true;
+            }
+
+            match message.as_str() {
+                // this is commonly thrown by infura and is apparently a load balancer issue, see also <https://github.com/MetaMask/metamask-extension/issues/7234>
+                "header not found" => true,
+                // also thrown by infura if out of budget for the day and ratelimited
+                "daily request count exceeded, request rate limited" => true,
+                //
+                "trace requests limited to 100/sec" => true,
+                _ => false,
+            }
+        }
+
+        match error {
+            ethers::providers::HttpClientError::ReqwestError(err) => {
+                err.status() == Some(http::StatusCode::TOO_MANY_REQUESTS)
+            }
+            ethers::providers::HttpClientError::JsonRpcError(err) => {
+                should_retry_json_rpc_error(err)
+            }
+            ethers::providers::HttpClientError::SerdeJson { text, .. } => {
+                // some providers send invalid JSON RPC in the error case (no `id:u64`), but the
+                // text should be a `JsonRpcError`
+                #[derive(Deserialize)]
+                struct Resp {
+                    error: JsonRpcError,
+                }
+
+                if let Ok(resp) = serde_json::from_str::<Resp>(text) {
+                    return should_retry_json_rpc_error(&resp.error);
+                }
+                false
+            }
+        }
+    }
+
+    fn backoff_hint(&self, error: &ethers::providers::HttpClientError) -> Option<Duration> {
+        if let ethers::providers::HttpClientError::JsonRpcError(JsonRpcError { data, .. }) = error {
+            let data = data.as_ref()?;
+
+            // if daily rate limit exceeded, infura returns the requested backoff in the error
+            // response
+            let backoff_seconds = &data["rate"]["backoff_seconds"];
+            // infura rate limit error
+            if let Some(seconds) = backoff_seconds.as_u64() {
+                return Some(Duration::from_secs(seconds));
+            }
+            if let Some(seconds) = backoff_seconds.as_f64() {
+                return Some(Duration::from_secs(seconds as u64 + 1));
+            }
+        }
+
+        None
+    }
+}
